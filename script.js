e3530a4ab4682f2d76b9bdfd54ab6b88b70d6c00
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-20 18:53:00 -0400

    added uv index and fixed styling

--- script.js
+++ script.js
@@ -43,33 +43,57 @@ var getCityInfo = function() {
         // } else {
         //     alert("Error: " + response.statusText);
         // }
-        var cityTime = new Date(data.dt * 1000);
+        var cityTime = moment(data.dt * 1000).format("MM/DD/YYYY");
 
-        var cityInfoName = document.createElement("p");
-        cityInfoName.textContent = data.name + cityTime;
+        var cityInfoName = document.createElement("h3");
+        cityInfoName.textContent = data.name + " (" + cityTime + ")";
         //$(cityInfoName).addClass("card-title");
         cityInfo.appendChild(cityInfoName);
 
-        var cityInfoList1 = document.createElement("p");
-        var cityInfoList2 = document.createElement("p");
-        var cityInfoList3 = document.createElement("p");
+        var cityInfoList1 = document.createElement("h6");
+        var cityInfoList2 = document.createElement("h6");
+        var cityInfoList3 = document.createElement("h6");
         cityInfoList1.textContent = "Temp: " + data.main.temp + "°C";
         cityInfoList2.textContent = "Wind: " + data.wind.speed + " MPH";
         cityInfoList3.textContent = "Humidity: " + data.main.humidity + "%"; 
         
-        
-        
-        // {
-        //     if (uvIndex >= 5) {
-        //         cityInfoList3.setAttribute = ("style", "background-color: green");
-        //     }
-            
-        // }
         cityInfo.appendChild(cityInfoList1);
         cityInfo.appendChild(cityInfoList2);
-        cityInfo.appendChild(cityInfoList3);
+        cityInfo.appendChild(cityInfoList3);      
 
-    })
+        var uvIndexUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey;
+
+        fetch(uvIndexUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+                
+                var cityInfoList4 = document.createElement("h6");
+                var cityInfoList5 = document.createElement("span");
+                cityInfoList4.textContent = "UV Index: "; 
+                cityInfoList5.textContent = data.current.uvi; 
+
+                if (data.current.uvi <= 2) {
+                    cityInfoList5.setAttribute('style', 'background-color:green; padding: 2px 8px; color: white; border-radius: 5px;'); 
+                }   
+                if (data.current.uvi > 3) {
+                    cityInfoList5.setAttribute('style', 'background-color:orange; padding: 2px 8px; color: white; border-radius: 5px;'); 
+                }
+                if (data.current.uvi > 8) {
+                    cityInfoList5.setAttribute('style', 'background-color:red; padding: 2px 8px; color: white; border-radius: 5px;'); 
+                }
+                
+
+                cityInfo.appendChild(cityInfoList4);
+                cityInfoList4.appendChild(cityInfoList5);
+
+
+            })
+        });
+        
+    
         // .catch(function(error) {
         // alert("Unable to connect to Open Weather");
         //});
