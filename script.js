bd544a9ef94a97001af41d9dfa397739393d7bac
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-19 23:37:18 -0400

    fetch url to get current city weather

--- script.js
+++ script.js
@@ -1,44 +1,81 @@
 var searchBtn = document.querySelector(".btn");
-var cityInput = document.querySelector("#cityName");
+var cityInput = document.querySelector("#cityName").value;
+var cityInfo = document.querySelector("#cityInfo");
+//var city = cityInput.value.trim();
+
 var apiKey = "02a19a3f81c6935cc3484c6eeb936427";
+var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?q=toronto" + "&appid=" + apiKey + "&units=metric";
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
 
-    var city = cityInput.value.trim();
+ for (var i = 0; i < localStorage.length; i++) {
 
-    if (city) {
-        getCityInfo(city);
+    cityInput = localStorage.getItem(i);
+     // console.log(localStorage.getItem("cityInput"));
+     var cityName = $(".list-group").addClass("list-group-item");
 
-        // clear old content
-        cityInput.textContent = "";
-    } else {
-        alert("Please enter a city name");
-    }    
-};
+     cityName.append("<li>" + cityInput + "</li>");
+ }
+
+ var formSubmitHandler = function(event) {
+     event.preventDefault();
+     // clear old content
+     cityInput.textContent = "";
+
+     if (cityInput === "") {
+         alert("Please enter a city name");
+
+     } else {
+         getCityInfo();
+     }    
+ };
 
 var getCityInfo = function() {
-    
-    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=metric";
 
     // make a get request to url
-    fetch(apiUrl)
+    fetch(apiCurrent)
         .then(function(response) {
-        // request was successful
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-            console.log(data);
-            displayRepos(data, user);
-            });
-        } else {
-            alert("Error: " + response.statusText);
-        }
+            return response.json();
         })
-        .catch(function(error) {
-        alert("Unable to connect to Open Weather");
-        });
+        .then(function(data) {
+             console.log(data);
+             //displayRepos(data, user);
+        //     });
+        // } else {
+        //     alert("Error: " + response.statusText);
+        // }
+        var cityTime = new Date(data.dt * 1000);
+
+        var cityInfoName = document.createElement("p");
+        cityInfoName.textContent = data.name + cityTime;
+        //$(cityInfoName).addClass("card-title");
+        cityInfo.appendChild(cityInfoName);
+
+        var cityInfoList1 = document.createElement("p");
+        var cityInfoList2 = document.createElement("p");
+        var cityInfoList3 = document.createElement("p");
+        cityInfoList1.textContent = "Temp: " + data.main.temp + "°C";
+        cityInfoList2.textContent = "Wind: " + data.wind.speed + " MPH";
+        cityInfoList3.textContent = "Humidity: " + data.main.humidity + "%"; 
+        
+        
+        
+        // {
+        //     if (uvIndex >= 5) {
+        //         cityInfoList3.setAttribute = ("style", "background-color: green");
+        //     }
+            
+        // }
+        cityInfo.appendChild(cityInfoList1);
+        cityInfo.appendChild(cityInfoList2);
+        cityInfo.appendChild(cityInfoList3);
+
+    })
+        // .catch(function(error) {
+        // alert("Unable to connect to Open Weather");
+        //});
 
 };
 
-searchBtn.addEventListener("click", formSubmitHandler);
+getCityInfo();
+
+searchBtn.addEventListener("click", getCityInfo);
